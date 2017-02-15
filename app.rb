32a67b5637e0564a5f9a247a064d8c8a5396c229
doverh <doverheld@gmail.com>
2017-02-15 16:33:58 -0500

    Creating app

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require_relative 'calculate.rb'
+
+
+get '/' do
+	
+	erb:name
+
+end
+
+post '/get_name' do
+ 	 user_name = params['user_name']
+ 	 erb:age, :locals => {:user_name=>user_name}
+end
+
+post '/get_age' do
+ 	 user_age = params['user_age']
+ 	 user_name = params['user_name']
+ 	 erb:favorite_numbers, :locals => {:user_age=>user_age,:user_name=>user_name}
+end
+
+post '/get_favorites' do
+ 	 user_age = params['user_age']
+ 	 user_name = params['user_name']
+
+ 	 # num1 = params['num1']
+ 	 # num2 = params['num2']
+ 	 # num3 = params['num3']
+ 	 # sum = sum_favorite(num1,num2,num3)
+ 	 # result = greater_than_age(sum,user_age)
+ 	 erb:results, :locals => {:user_age=>user_age,:user_name=>user_name}
+end
+
+
+
+
+	
+
